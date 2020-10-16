6cabed6e0a2fc895d24c4fbd856517ecf06ccc27
Agnes Chueng <agneschueng@gmail.com>
2020-10-15 19:13:42 -0500

    starter code with some pseudo code

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
 
 ];
 
-// function to write README file
+// function to write README file, need to require built in fs
 function writeToFile(fileName, data) {
+    // something to do with file --> README.md
+    generateMarkdown(data);
 }
 
 // function to initialize program
 function init() {
+    // ask inquirer stuff -> use inquirer.prompt(questions).then(data)
+    // call function writeToFile(fileName, data);
+        // fileName will be README.md, data will reference the response from asking inquirer stuff
+
 
 }
 
