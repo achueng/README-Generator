8674e1bcf1805aa9e84c31b43b9a0fad22261d86
Agnes Chueng <agneschueng@gmail.com>
2020-10-15 22:27:11 -0500

    created getBadge function to implement switch to return the correct badge string, and called it in promise.then() function

--- index.js
+++ index.js
@@ -67,6 +67,31 @@ const questions = [
 
 // console.log(questions);
 
+// function to return string to add to README
+// License badges from: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+function getBadge(license){
+    switch (license) {
+        case "Apache 2.0":
+            return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+            break;
+        case "Boost 1.0":
+            return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+            break;
+        case "GNU GPL v3":
+            return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+            break;
+        case "MIT":
+            return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+            break;
+        case "Mozilla 2.0":
+            return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+            break;
+        default:
+            break;
+    };
+}
+
+
 // function to write README file, need to require built in fs
 function writeToFile(fileName, data) {
     // use fs.writeFile() to write file --> README.md
@@ -86,6 +111,8 @@ function init() {
     .prompt(questions)
     .then(function(data) {
         // console.log(data); // yields object
+        // call getBadge function to add badge to markdown file
+        licenseBadge = getBadge(data.license);
         // call function writeToFile(fileName, data);
         writeToFile("README.md", data);
         // fileName will be README.md, data will reference the response from asking inquirer stuff
