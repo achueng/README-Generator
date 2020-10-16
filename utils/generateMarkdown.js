640ac02f554ed74604a91eed6c3c4cda288fe0b5
Agnes Chueng <agneschueng@gmail.com>
2020-10-15 19:13:16 -0500

    added basic layout to be created

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,30 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ## Table of Contents
+    1. Installation
+    2. Usage
+    3. License
+    4. Contributing
+    5. Tests
+    6. Questions
+
+  ## Installation
+
+  ## Usage
+
+  ## License
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
+
+
+
 `;
 }
 
